89d0073e1e61e1f62b66dc98d1740e8c462d38f6
Federico Sismondi <federico.sismondi@camptocamp.com>
2021-03-16 16:36:31 +0100

    Test upgrade keycloak operator

--- tests/k3s-docker/terraform/variables.tf
+++ tests/k3s-docker/terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "repo_url" {
   type    = string
-  default = "https://github.com/camptocamp/camptocamp-devops-stack.git"
+  default = "https://github.com/fsismondi/camptocamp-devops-stack.git"
 }
 
 variable "target_revision" {
   type    = string
-  default = "master"
+  default = "upgrade_keycloak_operator"
 }
